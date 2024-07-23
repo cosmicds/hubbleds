09556460b4d37059d611d4229d6d97b9d890d85b
Carifio24 <carifio24@gmail.com>
2024-07-23 16:19:33 -0400

    Pass local state into base layout.

--- src/hubbleds/layout.py
+++ src/hubbleds/layout.py
@@ -38,7 +38,7 @@ def Layout(children=[]):
             GLOBAL_STATE, LOCAL_STATE
         )
 
-        logger.info("Finished loading measurements.")
+        logger.info("Finished loading state.")
         loaded_states.set(True)
 
         Ref(LOCAL_STATE.fields.measurements_loaded).set(True)
@@ -70,6 +70,5 @@ def Layout(children=[]):
         children=children,
         story_name=LOCAL_STATE.value.story_id,
         story_title=LOCAL_STATE.value.title,
-        on_student_info_loaded=_on_student_info_loaded,
     ):
         pass
