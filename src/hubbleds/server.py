6fe18d3c4c3028300af5455e37266ae9d159b75b
Nicholas Earl <contact@nicholasearl.me>
2024-11-08 11:26:23 -0600

    Extend middleware to include cors handling

--- src/hubbleds/server.py
+++ src/hubbleds/server.py
@@ -3,7 +3,9 @@ from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Mount, Route
 from solara.server import settings
+from starlette.middleware import Middleware
 from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 import solara.server.starlette
 
@@ -18,13 +20,18 @@ routes = [
     # Mount("/", routes=solara.server.starlette.routes),
 ]
 
-app = Starlette(routes=routes)
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True),
+    Middleware(SessionMiddleware, secret_key="some-secret", max_age=None),
+]
+
+app = Starlette(routes=routes, middleware=middleware)
 
 from solara_enterprise.auth.starlette import oauth
 
 # TODO: `secret_key` defined here is for TESTING ONLY
-app.add_middleware(
-    SessionMiddleware,
-    secret_key="some-secret-key",
-    max_age=None,
-)
+# app.add_middleware(
+#     SessionMiddleware,
+#     secret_key="some-secret-key",
+#     max_age=None,
+# )
